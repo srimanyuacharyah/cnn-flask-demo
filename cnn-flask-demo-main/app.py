cd87f5597c4b2e92ad86207d0b90fb93f57274d0
srimanyu acharya h <pandushreyasp@gmail.com>
2026-02-02 12:42:28 +0530

    Add files via upload

---
+++ cnn-flask-demo-main/app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/abc')
+def abc():
+    return render_template('abc.html')
+@app.route('/<name>')
+def name(name):
+    return render_template('name.html',name=name,image=name+".png")
+
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
